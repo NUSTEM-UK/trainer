2b1df71691f716b7e9e14507da9db9f3dd3fddb7
jjsanderson <jonathan.sanderson@northumbria.ac.uk>
2021-10-06 15:38:26 +0100

    ESP side of software serial test

---
+++ experiments/softwareSerialParse/softwareSerialParse.ino
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define RX_PIN D2
+#define TX_PIN D1
+SoftwareSerial myPort(RX_PIN, TX_PIN, false, 256);
+
+String x;
+int i;
+
+void setup() {
+    Serial.begin(115200); // Standard hardware serial port, for debugging
+    myPort.begin(9600);
+    // myPort.begin(9600, SWSERIAL_8N1, D6, D6, false, 256);
+    if (!myPort) {
+        Serial.println("Invalid SoftwareSerial config");
+        while (1) {
+            // Don't continue with invalid config
+            delay (1000);
+        }
+    }
+    Serial.println();
+    Serial.println("SoftwareSerial started");
+    delay(500);
+
+}
+
+void loop() {
+    while (!myPort.available()) {
+        x = myPort.readString();
+        Serial.println(x);
+        if (x == "cabbage") {
+            Serial.println("...is horrid");
+            Serial.println(i++);
+        }
+    }
+}
