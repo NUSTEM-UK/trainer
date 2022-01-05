8a4aceee0a75fe7840937872143e99eefb73306a
Joe <joeshimwell@gmail.com>
2022-01-05 15:58:47 +0000

    Display workout

--- experiments/PicoDisplay/chicken_doctor.py
+++ experiments/PicoDisplay/chicken_doctor.py
@@ -1,5 +1,7 @@
 import picodisplay as display
 import utime
+import gc
+import machine
 
 # Set up and initialise Pico Display
 buf = bytearray(display.get_width() * display.get_height() * 2)
@@ -11,9 +13,69 @@ up_arrow =[0,4,14,21,4,4,0,0]
 down_arrow = [0,4,4,21,14,4,0,0]
 bits = [128,64,32,16,8,4,2,1]  # Powers of 2
 
+# Print defined character from set above
+def draw_char(xpos, ypos, pattern):
+    for line in range(8):  # 5x8 characters
+        for ii in range(5): # Low value bits only
+            i = ii + 3
+            dot = pattern[line] & bits[i] # Extract bit
+            if dot: # print white dots
+                display.pixel(xpos+i*2, ypos+line*2)
+                display.pixel(xpos+i*2, ypos+line*2+1)
+                display.pixel(xpos+i*2+1, ypos+line*2)
+                display.pixel(xpos+i*2+1, ypos+line*2+1)
+
+def backround_draw(top_left, top_right, bottom_left, bottom_right):
+    global button_a
+    global button_b
+    global button_x
+    global button_y
+
+    if button_a == 1:
+        display.set_pen(0,255,0)
+    else:
+        display.set_pen(255,255,0)
+    display.text(str(top_left), 10, 25,200)
+    
+    if button_x == 1:
+        display.set_pen(0,255,0)
+    else:
+        display.set_pen(255,255,0)    
+    display.text(str(top_right), 200, 25,200)
+    if button_b == 1:
+        display.set_pen(0,255,0)
+    else:
+        display.set_pen(255,255,0)
+    display.text(str(bottom_left), 10, 100,200)
+    
+    if button_y == 1:
+        display.set_pen(0,255,0)
+    else:
+        display.set_pen(255,255,0)    
+    display.text(str(bottom_right), 200, 100,200)
+    #Draw the Top Horizontal Scale
+    display.set_pen(255,255,255)
+    display.pixel_span(50,31,140)
+    display.pixel_span(50,32,140)
+    display.pixel_span(50,105,140)
+    display.pixel_span(50,106,140)
+
+    #calculate the pixel positions of the ticks
+    top_interval = [top_left, top_right] 
+    bottom_interval = [bottom_left, bottom_right]
+
+    top_absolutes = (int(50 + ((top_interval[0]/180)*140)), int(50 + ((top_interval[1]/180)*140)))
+    bottom_absolutes = (int(50 + ((bottom_interval[0]/180)*140)), int(50 + ((bottom_interval[1]/180)*140)))
+
+    display.set_pen(255,255,255)
+    display.rectangle(top_absolutes[0], 27, 2, 12)
+    display.rectangle(top_absolutes[1], 27, 2, 12)
+    display.rectangle(bottom_absolutes[0], 100, 2, 12)
+    display.rectangle(bottom_absolutes[1], 100, 2, 12)
+
 # set the min and max x pixel positions
 x_min = 50
-x_max = 140
+x_max = 180
 
 # set the min and max x angular positions
 x_angle_min = 0
@@ -23,9 +85,9 @@ x_angle_max = 180
 cycle_time = 5000
 
 # set default angles for for the two servos - default swing between 0 and 180
-servo1_x_start = 0
+servo1_x_start = 30
 servo1_x_current = servo1_x_start
-servo1_x_end = 180
+servo1_x_end = 100
 
 servo2_x_start = 0
 servo2_x_current = servo2_x_start
@@ -36,11 +98,19 @@ start_time = utime.ticks_ms()
 end_time = start_time + cycle_time
 now_time = start_time 
 
+# set flags for button pressing
+button_a = 0
+button_x = 0
+button_b = 0
+button_y = 0
+
 print (start_time)
 print (end_time)
 
 # update function - how long has passed since last update, and where should we be now
 def update_animation():
+    display.set_pen(0, 0, 0)
+    display.clear()
     global start_time
     global end_time
     global now_time
@@ -56,30 +126,94 @@ def update_animation():
         # we're on the way up, as 1.0 would be a full cycle
         # use that proportion to work out where we should be angularly
         current_angle = servo1_x_start + (done_so_far * 2 * (servo1_x_end - servo1_x_start))
-        print ("Current angle is:", current_angle, sep=' ')
 
         # then use the angle to work out the pixl position of where we are
-        current_pixel = x_min + (current_angle / x_angle_max) * (x_max - x_min)
-        print ("Current pixel is:", current_pixel, sep=' ')
+        current_pixel = int(x_min + (current_angle / x_angle_max) * (x_max - x_min))
 
     elif 1.0 >= done_so_far >= 0.5:
         # we back the way down
         done_so_far = 1 - done_so_far
 
         current_angle = servo1_x_start + (done_so_far * 2 * (servo1_x_end - servo1_x_start))
-        print ("Current angle is:", current_angle, sep=' ')
 
         # then use the angle to work out the pixl position of where we are
-        current_pixel = x_min + (current_angle / x_angle_max) * (x_max - x_min)
-        print ("Current pixel is:", current_pixel, sep=' ')        
+        current_pixel = int(x_min + (current_angle / x_angle_max) * (x_max - x_min))
     else:
         # we've completed the cycle, so set new start and end times
         start_time = now_time
         end_time = start_time + cycle_time
-        print("Cycle Complete")
+        current_angle = servo1_x_start
+        current_pixel = int(x_min + (current_angle / x_angle_max) * (x_max - x_min))
+    backround_draw(servo1_x_start, servo1_x_end, servo2_x_start, servo2_x_end)
+    draw_char(current_pixel-6, 38, up_arrow)
+
+    display.update()
+
+def button_checker():
+    global button_a
+    global button_b
+    global button_x
+    global button_y
+
+    total = button_y + button_x + button_a + button_b
+
+    if display.is_pressed(display.BUTTON_A): 
+        if total == 0:
+            button_a = 1
+            print("A pressed")                                    
+            print(button_a)
+        else:
+            if button_a == 1:
+                button_a = 0
+                print("A pressed")                                    
+                print(button_a)
+
+        #debounce
+        utime.sleep_ms(500)
+    
+    if display.is_pressed(display.BUTTON_B):              # if a button press is detected then...   
+        if total == 0:
+            button_b = 1
+            print("B pressed")                                    
+            print(button_b)
+        else:
+            if button_b == 1:
+                button_b = 0
+                print("B pressed")                                    
+                print(button_b)
+        #debounce
+        utime.sleep_ms(500)
+    if display.is_pressed(display.BUTTON_X):              # if a button press is detected then...   
+        if total == 0:
+            button_x = 1
+            print("X pressed")                                    
+            print(button_x)
+        else:
+            if button_x == 1:
+                button_x = 0
+                print("X pressed")                                    
+                print(button_x)
+
+        #debounce
+        utime.sleep_ms(500)
+    
+    if display.is_pressed(display.BUTTON_Y):              # if a button press is detected then...   
+        if total == 0:
+            button_y = 1
+            print("Y pressed")                                    
+            print(button_y)
+        else:
+            if button_y == 1:
+                button_y = 0
+                print("Y pressed")                                    
+                print(button_y)
+        #debounce
+        utime.sleep_ms(500)
+
 
 
 while True:
+    button_checker()
     update_animation()
-    utime.sleep_ms(100)
+    utime.sleep_ms(10)
 
