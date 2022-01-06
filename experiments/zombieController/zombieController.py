3abdc630ec5cbea508ca9320fed1272c5088a6f4
jjsanderson <jonathan@quernstone.com>
2022-01-06 10:00:58 +0000

    Comments

--- experiments/zombieController/zombieController.py
+++ experiments/zombieController/zombieController.py
@@ -15,8 +15,16 @@ waitingForConnection = True
 isHarbinger = False # It's a Mass Effect reference
 
 def getConnection():
+    """Wait for serial connection and process handshake
+
+    Sends periodic 'ACK', waiting for an 'ACK ACK ACK' response.
+    Sets flag isHarbinger on successful handshake, and unsets
+    waitingForConnection.
+    """
     global waitingForConnection
     global isHarbinger
+
+    # We've been called, so assume we're not connected
     while waitingForConnection:
         uart1.write('ACK\n')
         print("ACK")
@@ -36,6 +44,31 @@ def getConnection():
         utime.sleep(delay)
 
 if __name__ == '__main__':
+    """Not necessarily written in the cleanest way for integration with Joe's work.
+
+    I think what I'd do is:
+    - Rewrite getConnection() as a checkForConnection() function, removing the loop from it
+    - Have getConnection read serial responses, but send its ACK query only on a time-since-last-called basis
+    - Note that serial read handles entire lines, so the existing code will probably work.
+    - In __main__, do something like:
+
+        while True:
+            if waitingForConnection:
+                checkSerialConnection()
+            # could probably be a plain else here, since we should be in one state or the other
+            if isHarbinger:
+                # Code as below, likely abstracted into a function
+            # Then whatever's needed for Joe's bit.
+            handleButtons()
+            updateDisplayAndServoData()
+            # ...or whatever
+
+    TODO: Would be nice if the system reverted to waitingForConnection if it's disconnected.
+          However, I'm not sure how we'd do that without constantly checking we still have
+          a connection... which the Arduino end doesn't currently handle. Which I think means
+          both ends of the serial connection will need to be restarted after a session.
+          Lack of reset button on a Pico is annoying here.
+    """
     while True:
         getConnection()
         while isHarbinger:
